0e087bacd12773c6c5e7c843dd3cd6bebe5d7d4e
Jozef Fulop jun <jozko@silake.com>
2015-07-14 15:07:14 +0200

    Pridane komentare a upravene vecicky

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,7 @@
 class Book < ActiveRecord::Base
+  has_many :comments
+
+  def short_description
+    description.truncate(20)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150712134017) do
+ActiveRecord::Schema.define(version: 20150714102343) do
 
   create_table "books", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "comment_text"
+    t.string   "reader_name"
+    t.integer  "book_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
 end
